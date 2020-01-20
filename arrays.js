2fc26fcdd80514d6f550b046cdf1e7e1918f149f
Daniel Paz <daniel@paztoya.com>
2020-01-20 22:40:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,6 +31,6 @@ function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
  function destructivelyRemoveElementFromEndOfArray(array){
-array = array.pop()
-return array
+  array = array.pop()
+  return array
  }
