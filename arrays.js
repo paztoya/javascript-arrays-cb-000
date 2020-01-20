22945b793e60565daa23dc702dc7fd7c51e6de68
Daniel Paz <daniel@paztoya.com>
2020-01-20 22:09:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, el){
-return [el, array]
+return [el, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(){
